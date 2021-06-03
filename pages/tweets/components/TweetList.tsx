9d9efeba7572d183cdecc0d965de0b47230af96b
brlaney42 <brlaney@outlook.com>
2021-06-03 12:26:29 -0400

    Updated next need to fix component textPie

--- pages/tweets/components/TweetList.tsx
+++ pages/tweets/components/TweetList.tsx
@@ -3,7 +3,12 @@
 const TweetList = ({ tweets }) => {
   return (
     <>
-      <div className='uk-flex uk-flex-center uk-child-width-1-1 uk-flex-wrap-around' uk-grid='true' uk-scrollspy='cls: uk-animation-fade; target: .uk-flex; delay: 175; repeat: false' style={{ width: '1000px' }}>
+      <div
+        uk-scrollspy='cls: uk-animation-fade; target: .uk-flex; delay: 175; repeat: false'
+        className='uk-flex uk-flex-center uk-child-width-1-1 uk-flex-wrap-around'
+        style={{ width: '1000px' }}
+        uk-grid='true'
+      >
         {tweets.map((tweet) => (
           <div key={tweet.id} style={{ fontFamily: 'Rubik, sans-serif' }}>
             <TweetItem tweet={tweet} />
